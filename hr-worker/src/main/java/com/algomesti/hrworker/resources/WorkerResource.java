81f95b00aa66a9a711aaec211b2160f02af1dd06
Alexandre Gomes <alexandre.gomes@imusica.com.br>
2021-08-06 17:03:12 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/algomesti/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/algomesti/hrworker/resources/WorkerResource.java
@@ -36,7 +36,12 @@ public class WorkerResource {
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);	
 	}
